06eccb81081dd0f7d5d3c73c04f856056163b36a
s0lena <solena.work@gmail.com>
2024-03-26 15:29:50 +0200

    Done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+document.querySelector(".question").textContent =
+  "Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.";
 
+const answer = prompt("Vase odpoved");
+
+document.querySelector(".answer__text").textContent = answer;
+
+if (answer === "Bell") {
+  document.querySelector(".answer").classList.add("answer--correct");
+} else {
+  document.querySelector(".answer").classList.add("answer--wrong");
+}
